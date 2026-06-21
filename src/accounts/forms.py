6b21d29887dfe276a9c88423a8a8dd6a0eb1ba2c
Nihal-123-456 <knasn1526@example.com>
2026-06-21 22:00:49 +0600

    edited several pages

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
 User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            'username', 'email', 'role', 'first_name', 'last_name', 'password1', 'password2',
+            Submit('submit', 'Register Now', css_class='btn btn-primary w-100 py-2 mt-3 fw-bold rounded-pill')
+        )
         
         placeholders = {
             'username': 'Enter your username',
@@ -19,8 +28,13 @@ class UserRegisterForm(UserCreationForm):
         
         for field_name, field in self.fields.items():
             field.widget.attrs.update({
-                'placeholder': placeholders.get(field_name, f'Enter {field_name}')
+                'placeholder': placeholders.get(field_name, f'Enter {field_name}'),
+                'class': 'form-control'
             })
+
+        self.fields['username'].help_text = '' 
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
     role = forms.ChoiceField(choices=User.ROLE_CHOICES)
 
     class Meta:
@@ -36,6 +50,15 @@ class UserLoginForm(forms.Form):
             'class': 'form-control',
             'placeholder': 'Enter your password'
         }))
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            'email', 'password',
+            Submit('submit', 'Sign In', css_class='btn btn-primary w-100 py-2 mt-3 fw-bold rounded-pill')
+        )
 
 class JobSeekerProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
